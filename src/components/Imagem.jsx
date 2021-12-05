83d717f1e2ae88de7f1e33c31d9913a87b211193
priKSouza <priscilakarinajump@gmail.com>
2021-12-05 17:29:14 -0300

    atualiação para realizar deploy

--- src/components/Imagem.jsx
+++ src/components/Imagem.jsx
@@ -1,4 +1,4 @@
-function Imagem (props){
+function Imagem (git add .){
     return(
         <a className="img-footer" href="https://github.com/priKSouza" target="_blank" rel="noreferrer">
             <img className="img" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" /></a>    
