261ac8a8d9a86cab899647bcd818d5aa95c3a1a4
priKSouza <priscilakarinajump@gmail.com>
2021-12-05 17:19:11 -0300

    iniciando deploy para o netlify

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = () => {
         {music.map(music =>
             <div className="card-principal" key={music.id}>
                 <h2 className="title">{music.musica}</h2>
-                <a className="link" target="_blank" href={music.link}>Escute aqui 🎧</a>
+                <a className="link" target="_blank" rel="noreferrer" href={music.link}>Escute aqui 🎧</a>
                 <p className="name">{music.trecho}</p>
                 <img className="image" src={music.capa} alt={music.capa} />
             </div>
--- src/components/Imagem.jsx
+++ src/components/Imagem.jsx
@@ -1,8 +1,7 @@
-function Imagem (){
+function Imagem (props){
     return(
-        <a className="img-footer" href="https://github.com/priKSouza" target="_blank">
-            <img className="img" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" /> Priscila Souza
-        </a>    
+        <a className="img-footer" href="https://github.com/priKSouza" target="_blank" rel="noreferrer">
+            <img className="img" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" /></a>    
     )
 }
 
