dc9f988f5ebcb6e772a092880262c7c48e03708a
priKSouza <priscilakarinajump@gmail.com>
2021-12-05 17:45:00 -0300

    atualiando para fazer deploy

--- src/components/Imagem.jsx
+++ src/components/Imagem.jsx
@@ -1,7 +1,10 @@
+import { VscGithubInverted } from 'react-icons/vsc' 
+
 function Imagem (){
     return(
         <a className="img-footer" href="https://github.com/priKSouza" target="_blank" rel="noreferrer">
-            <img className="img" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" /></a>    
+            <VscGithubInverted/>
+        </a>    
     )
 }
 
